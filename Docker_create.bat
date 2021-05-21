a2136ed7514533e1b803672f75561a655d1ad19e
vernon.dsouza123 <vernon.dsouza123@gmail.com>
2021-05-21 15:06:13 +0200

    Correction in docker bat

--- Docker_create.bat
+++ Docker_create.bat
@@ -1,6 +1,8 @@
 ECHO OFF
 docker image build -t prediction .
 docker run -p 5001:5000 -d prediction
-docker push vernondsouza/prediction:tagname
+docker login
+docker tag prediction:latest vernondsouza/prediction
+docker push vernondsouza/prediction
 ECHO Go to http://localhost:5001 in a browser to see the results
 PAUSE
